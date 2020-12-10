7183b0d08c1650ef761ab69e33a217ea13142a67
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-09 16:48:35 -0800

    define isPangram function

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,13 @@
 const isPangram = function(text) {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    text = text.toLowerCase()
 
+    for (const letter of alphabet) {
+        if (!text.includes(letter)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 module.exports = isPangram;
